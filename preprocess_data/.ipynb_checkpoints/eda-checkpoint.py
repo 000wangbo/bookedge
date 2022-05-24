72c5d5c5de8f4a03d3ebe56f124b097d8d60afa1
000wangbo <000wangbo@163.com>
2022-05-25 01:27:48 +0800

    nits

--- preprocess_data/.ipynb_checkpoints/eda-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import os 
-import numpy as np
-for i in os.listdir('/data/jupyter/bookedge/train_datasets_document_detection_0411/segments/'):
-    img = cv2.imread('/data/jupyter/bookedge/train_datasets_document_detection_0411/segments/' + i)
-    if len(np.unique(img)) > 2:
-        print('asdasdads')
